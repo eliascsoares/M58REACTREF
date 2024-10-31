8b5a3e3aea2a6fc2438e21b5c4fd17adc7a56ab9
eliascsoares <40612619+eliascsoares@users.noreply.github.com>
2024-10-31 09:56:59 +0000

    Added Login Logout function

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,20 @@ import { useState } from 'react'
 
 
 function App() {
-const [arrayOfPictures, setArrayofPictures] = useState([]);
+const [arrayOfPictures, setArrayOfPictures] = useState([]);
+const [isLoggedIn, setIsLoggedIn] = useState(false);
 
 async function fetchImages() {
   const fetchImages = await fetch("https://picsum.photos/v2/list?page=3&limit=25");
   const dataReceived = await fetchImages.json();
-  setArrayofPictures(dataReceived);
+  setArrayOfPictures(dataReceived);
 }
+
+function loginLogout() {
+  setIsLoggedIn(!isLoggedIn);
+  console.log(isLoggedIn);
+}
+
 
 useEffect(() => {
   fetchImages()
@@ -20,16 +27,20 @@ useEffect(() => {
 
   return (
     <>
-      <button onClick={(event) => fetchImages()}>Get Images</button>
+      <button onClick={() => loginLogout()}>{isLoggedIn ? <p>Logout</p> : <p>Login</p>}</button>
+      <br />
       <h1>My React Refresh App</h1>
+    {isLoggedIn && 
+    <> 
       {arrayOfPictures.map((item, index) => {
         return (
           <Post url={item.download_url} author={item.author}></Post>
         )
-      })}      
-
+      })}
+    </>  
+    }
     </>
   )
-}
+  }
 
 export default App
